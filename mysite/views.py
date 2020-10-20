b23033d634a2d33c59d565c367d3b6cbe6b3ccc5
Chen BO-YI <johnny170889@Chende-MacBook-Pro.local>
2020-10-20 14:07:06 +0800

    first commit

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+def index (request):
+	name = "CHEN"
+	lottos= list()
+	for i in range(6):
+		lottos.append(random.randint(1,42))
+	return render(request, 'index.html', locals())
